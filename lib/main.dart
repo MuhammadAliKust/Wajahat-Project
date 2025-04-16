7c84ced39d43de9efb48ba9f7c5474ba93c5eb15
Muhammad Ali Nizami <kustalics@gmail.com>
2025-04-16 11:54:10 +0500

    Body and FAB Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
+void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,20 +12,47 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.green,
         appBar: AppBar(
-          title: Text('Hello World',style: TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold
-          ),),
+          title: Text(
+            'Hello World',
+            style: TextStyle(
+                fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
           centerTitle: true,
           backgroundColor: Colors.yellow,
-          leading: Icon(Icons.arrow_back,color: Colors.white,),
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
           actions: [
-            Icon(Icons.notifications,color: Colors.blue,),
-            Icon(Icons.shopping_cart,color: Colors.blue,),
-            Icon(Icons.add,color: Colors.blue,),
+            Icon(
+              Icons.notifications,
+              color: Colors.blue,
+            ),
+            Icon(
+              Icons.shopping_cart,
+              color: Colors.blue,
+            ),
+            Icon(
+              Icons.add,
+              color: Colors.blue,
+            ),
           ],
+        ),
+        body: Center(
+            child: Text(
+          "First Screen",
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.yellow),
+        )),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          onPressed: () {},
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
         ),
       ),
     );
