c8246149ccc78d2c19f2b73e07e0ba97b08ad84c
Muhammad Ali Nizami <kustalics@gmail.com>
2025-04-23 10:35:50 +0500

    Network Image Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'network_image.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,105 +13,106 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.green,
-        appBar: AppBar(
-          title: Text(
-            'Hello World',
-            style: TextStyle(
-                fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.yellow,
-          leading: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          actions: [
-            Icon(
-              Icons.notifications,
-              color: Colors.blue,
-            ),
-            Icon(
-              Icons.shopping_cart,
-              color: Colors.blue,
-            ),
-            Icon(
-              Icons.add,
-              color: Colors.blue,
-            ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "First Text",
-              style: TextStyle(fontSize: 30),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Second Text",
-              style: TextStyle(fontSize: 20),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.close),
-                Column(
-                  children: [
-                    Icon(Icons.close),
-                    Icon(Icons.close),
-                    Icon(Icons.close),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.close),
-                    Row(
-                      children: [
-                        Icon(Icons.close),
-                        Icon(Icons.close),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.close),
-                        Icon(Icons.close),
-                        Icon(Icons.close),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.close),
-                        Icon(Icons.close),
-                      ],
-                    ),
-                    Icon(Icons.close),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.close),
-                    Icon(Icons.close),
-                  ],
-                ),
-                Icon(Icons.close),
-              ],
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue,
-          onPressed: () {},
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      home: NetworkImageDemo(),
+      // home: Scaffold(
+      //   backgroundColor: Colors.green,
+      //   appBar: AppBar(
+      //     title: Text(
+      //       'Hello World',
+      //       style: TextStyle(
+      //           fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
+      //     ),
+      //     centerTitle: true,
+      //     backgroundColor: Colors.yellow,
+      //     leading: Icon(
+      //       Icons.arrow_back,
+      //       color: Colors.white,
+      //     ),
+      //     actions: [
+      //       Icon(
+      //         Icons.notifications,
+      //         color: Colors.blue,
+      //       ),
+      //       Icon(
+      //         Icons.shopping_cart,
+      //         color: Colors.blue,
+      //       ),
+      //       Icon(
+      //         Icons.add,
+      //         color: Colors.blue,
+      //       ),
+      //     ],
+      //   ),
+      //   body: Column(
+      //     mainAxisAlignment: MainAxisAlignment.start,
+      //     children: [
+      //       Text(
+      //         "First Text",
+      //         style: TextStyle(fontSize: 30),
+      //       ),
+      //       SizedBox(
+      //         height: 20,
+      //       ),
+      //       Text(
+      //         "Second Text",
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //         children: [
+      //           Icon(Icons.close),
+      //           Column(
+      //             children: [
+      //               Icon(Icons.close),
+      //               Icon(Icons.close),
+      //               Icon(Icons.close),
+      //             ],
+      //           ),
+      //           Column(
+      //             children: [
+      //               Icon(Icons.close),
+      //               Row(
+      //                 children: [
+      //                   Icon(Icons.close),
+      //                   Icon(Icons.close),
+      //                 ],
+      //               ),
+      //               Row(
+      //                 children: [
+      //                   Icon(Icons.close),
+      //                   Icon(Icons.close),
+      //                   Icon(Icons.close),
+      //                 ],
+      //               ),
+      //               Row(
+      //                 children: [
+      //                   Icon(Icons.close),
+      //                   Icon(Icons.close),
+      //                 ],
+      //               ),
+      //               Icon(Icons.close),
+      //             ],
+      //           ),
+      //           Column(
+      //             children: [
+      //               Icon(Icons.close),
+      //               Icon(Icons.close),
+      //             ],
+      //           ),
+      //           Icon(Icons.close),
+      //         ],
+      //       )
+      //     ],
+      //   ),
+      //   floatingActionButton: FloatingActionButton(
+      //     backgroundColor: Colors.blue,
+      //     onPressed: () {},
+      //     child: Icon(
+      //       Icons.add,
+      //       color: Colors.white,
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
